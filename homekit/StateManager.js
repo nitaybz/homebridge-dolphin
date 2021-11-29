7d83f3e2176ebca7c58b9b199702c94408bb9344
Nitay Ben-Zvi <nitaybz@gmail.com>
2021-11-29 18:09:29 +0200

    remove state call after set

--- homekit/StateManager.js
+++ homekit/StateManager.js
@@ -11,27 +11,22 @@ module.exports = (device, platform) => {
 			deviceCache.Temperature = state.Temperature
 		if (state.targetTemperature && deviceCache.targetTemperature !== state.targetTemperature)
 			deviceCache.targetTemperature = state.targetTemperature
-		
-		deviceCache.showerTemperature = state.showerTemperature
+		if (state.showerTemperature)
+			deviceCache.showerTemperature = state.showerTemperature
 	}
 
-	const updateState = () => {
-		dolphinApi.getState(device.deviceName)
-			.then(state => {
-				updateDeviceCache(state)
-				if (state.Power === 'OFF') {
-					device.ThermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(0)
-					device.ThermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(0)
-				} else {
-					device.ThermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(1)
-					device.ThermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(1)
-					device.ThermostatService.getCharacteristic(Characteristic.TargetTemperature).updateValue(state.targetTemperature)
-				}
-				device.ThermostatService.getCharacteristic(Characteristic.CurrentTemperature).updateValue(state.Temperature)
-			})
-			.catch(err => {
-				log(err)
-			})
+	const updateState = (res) => {
+		const state = res.Status
+		updateDeviceCache(state)
+		if (state.Power === 'OFF') {
+			device.ThermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(0)
+			device.ThermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(0)
+		} else {
+			device.ThermostatService.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(1)
+			device.ThermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(1)
+			device.ThermostatService.getCharacteristic(Characteristic.TargetTemperature).updateValue(state.targetTemperature)
+		}
+		device.ThermostatService.getCharacteristic(Characteristic.CurrentTemperature).updateValue(state.Temperature)
 	}
 
 	return {
@@ -100,28 +95,18 @@ module.exports = (device, platform) => {
 				if (!state) {
 					log.easyDebug(`Turning OFF Device ${device.deviceName}`)
 					return dolphinApi.turnOff(device.deviceName)
-						.finally(updateState)
-				} else if (deviceCache.targetTemperature && deviceCache.targetTemperature < deviceCache.Temperature) {
-					log.easyDebug(`Turning OFF Device ${device.deviceName} - Target Temperature is Lower than Current Temperature`)
-					return dolphinApi.turnOff(device.deviceName)
-						.finally(updateState)
+						.then(updateState)
 				} else {
 					log.easyDebug(`Turning ON Device ${device.deviceName} with Temperature ${deviceCache.targetTemperature || 37}ºC`)
 					return dolphinApi.turnOn(device.deviceName, deviceCache.targetTemperature || 37)
-						.finally(updateState)
+						.then(updateState)
 				}
 			},
 		
 			TargetTemperature: (temp) => {
-				if (temp < deviceCache.Temperature) {
-					log.easyDebug(`Turning OFF Device ${device.deviceName} - Target Temperature is Lower than Current Temperature`)
-					return dolphinApi.turnOff(device.deviceName)
-						.finally(updateState)
-				}
-			
 				log.easyDebug(`Setting Device ${device.deviceName} with Temperature ${temp}ºC`)
 				return dolphinApi.turnOn(device.deviceName, temp)
-					.finally(updateState)
+					.then(updateState)
 			},
 		}
 	}
